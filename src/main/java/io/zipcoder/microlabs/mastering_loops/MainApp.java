a670261952ca149ad659217b89602270cc2842e7
nathaneckel <nathan@patientpaperwork.com>
2024-03-04 21:00:16 -0500

    initial read of files and tests - will sleep and improve tomorrow

--- src/main/java/io/zipcoder/microlabs/mastering_loops/MainApp.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/MainApp.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 /**
  * Created by leon on 1/31/18.
  */
+
+//initial read/view of all files/tests.  Will sleep on this tonight and improve tomorrow
+    //initial commit this evening
 public class MainApp {
     public static void main(String[] args) {
     }
